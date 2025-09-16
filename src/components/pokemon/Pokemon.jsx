506a65bb8fe8efc3aa22a70502dd85b8a7be92b0
dayattt111 <tumbaldaya@gmail.com>
2025-09-17 02:22:30 +0800

    new: func Fetch All Data PokeApi

---
+++ src/components/pokemon/Pokemon.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Pokemon() {
+    // const [pokeList, setPokeList] = useState([]);
+  async function getAllPoke(){
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon"
+    // const restData = await fetch(apiUrl);
+    // const jsonData =  await restData.json();
+
+    // console.log(jsonData);
+    // setPokeList(jsonData);
+  }
+  
+  getAllPoke();
+//   console.log(pokeList);
+  
+    return (
+    <div>Pokemon aja</div>
+  )
+}
+
+export default Pokemon
