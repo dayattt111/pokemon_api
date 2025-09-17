3d1b44609585fca3dad5b0ed2ee2f34bb01895b4
dayattt111 <tumbaldaya@gmail.com>
2025-09-18 03:13:41 +0800

    add: some folder and func Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import "./App.css"
-import Pokemon from "./components/pokemon/Pokemon"
+import React, {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { getPokemon, getAllPokemon } from './Services/pokeService'
+import PokemonList from '../src/Pages/pokemonList'
+import './App.css';
 
-const App = () => {
+function App() {
+  const [pokemonData, setPokemonData] = useState([])
+  const [loading, isLoading] = useState(true)
+  const apiURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(apiURL)
+      await loadPokemon(response.results)
+      isLoading(false)
+      console.log(response)
+    }
+    fetchData()
+  }, [])
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonGet = await getPokemon(pokemon)
+      return pokemonGet
+    }))
+    setPokemonData(_pokemonData)
+  }
+
   return (
-    // <div>App</div>
     <>
-      <Pokemon />
+    <Router>
+      <div className='gridContainer'>
+        {loading ? <h1>Loading...</h1> : (
+          <Route exact path='/'>
+            {pokemonData.map((pokemon, i) => {
+              return <PokemonList key={i} pokemon={pokemon} />
+            })}
+          </Route>
+        )}
+      </div>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
