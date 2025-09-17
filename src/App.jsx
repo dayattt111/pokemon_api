1d67bb3115bff3b2090239e4a6c49d82cf73d81e
dayattt111 <tumbaldaya@gmail.com>
2025-09-18 03:38:37 +0800

    error: diplay blank, maybe a need a coffe

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,56 @@
-import React, {useState, useEffect} from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { getPokemon, getAllPokemon } from './Services/pokeServices'
 import PokemonList from './Pages/pokeList'
 import './App.css';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([])
-  const [loading, isLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const apiURL = 'https://pokeapi.co/api/v2/pokemon'
 
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemon(apiURL)
       await loadPokemon(response.results)
-      isLoading(false)
+      setLoading(false)
       console.log(response)
     }
     fetchData()
   }, [])
 
   const loadPokemon = async (data) => {
-    let _pokemonData = await Promise.all(data.map(async pokemon => {
-      let pokemonGet = await getPokemon(pokemon)
-      return pokemonGet
-    }))
+    let _pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonGet = await getPokemon(pokemon)
+        return pokemonGet
+      })
+    )
     setPokemonData(_pokemonData)
   }
 
   return (
-    <>
     <Router>
       <div className='gridContainer'>
-        {loading ? <h1>Loading...</h1> : (
-          <Route exact path='/'>
-            {pokemonData.map((pokemon, i) => {
-              return <PokemonList key={i} pokemon={pokemon} />
-            })}
-          </Route>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  {pokemonData.map((pokemon, i) => (
+                    <PokemonList key={i} pokemon={pokemon} />
+                  ))}
+                </>
+              }
+            />
+          </Routes>
         )}
       </div>
     </Router>
-    </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Pages/pokeList.jsx
+++ src/Pages/pokeList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@mui/styles";
+import { styled } from '@mui/material/styles';
+import Grid from "@mui/material/Grid";
 import pokeColor from '../Assets/colorPoke'
 import '../Assets/css/styled.css'
 
